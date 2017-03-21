2e65b97f0f2f570e7aeb7878d906ba2a77b84bdc
Shkodenko V. Taras <podlom@users.noreply.github.com>
2017-03-21 11:28:59 +0200

    Update 13.php

--- 13.php
+++ 13.php
@@ -7,17 +7,11 @@ $rows = 10;
 $cols = 10;
 
 echo '<table border="1">';
-
-for ($tr = 1; $tr <= $rows; $tr++)
-    {
-        echo "<tr>";
-
-        for ($td = 1; $td <= $cols; $td++)
-            {
-                echo '<td>' , $tr * $td, '</td>';
-            }
-
-        echo '</tr>';
-
+for ($tr = 1; $tr <= $rows; $tr++) {
+	echo "<tr>";
+	for ($td = 1; $td <= $cols; $td++) {
+		echo '<td>' , $tr , ' * ' , $td , ' = ' , $tr * $td, '</td>';
+	}
+	echo '</tr>';
 }
 echo '</table>';
