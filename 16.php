8a8483c30ab8ae7652cff7c40aa660096b947b4c
Shkodenko V. Taras <podlom@users.noreply.github.com>
2017-03-21 11:33:30 +0200

    Update 16.php

--- 16.php
+++ 16.php
@@ -9,10 +9,9 @@
 
 $arr = [1, 2, 3, 4, 5, 6, 7, 8,9];
 
-foreach ($arr as $value)
-    {
-        if ($value == 3 || $value == 6 || $value == 9)
-            echo $value, '<br>';
-        else
-            echo $value, ', ';
-    }
+foreach ($arr as $value) {
+	if ($value % 3 == 0)
+		echo $value, '<br>';
+	else
+		echo $value, ', ';
+}
